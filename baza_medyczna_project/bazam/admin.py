2844117643d03f2443cc7805ed8cea5f8b1e2d52
ffraczek <ffraczek@st.swps.edu.pl>
2026-01-16 15:27:30 +0100

    baza bazy 3

--- baza_medyczna_project/bazam/admin.py
+++ baza_medyczna_project/bazam/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import Doctor, MedicalProcedures
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ["imie" , "nazwisko" , "stanowisko"]
+    list_filter = ["stanowisko" , "data_dodania"]
+
+admin.site.register(Osoba, OsobaAdmin)
+
+from .models import Doctor, MedicalProcedures, Osoba
 
 admin.site.register(Doctor)
 admin.site.register(MedicalProcedures)
+
+
