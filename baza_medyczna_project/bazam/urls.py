dde48aaac99513bfb50720b84896c992d55898cc
jrepelowicz <jrepelowicz@st.swps.edu.pl>
2026-01-23 09:54:44 +0100

    fe2

--- baza_medyczna_project/bazam/urls.py
+++ baza_medyczna_project/bazam/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.strona_glowna, name='strona_glowna'),
+
     path('lekarze/', views.lista_doktorow, name='lista_doktorow'),
 ]
--- baza_medyczna_project/bazam/views.py
+++ baza_medyczna_project/bazam/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 
 def lista_doktorow(request):
-<<<<<<< HEAD
-    doctors = Doctor.objects.all()
-    return render(request, 'bazam/lista_doktorow.html', {'doctors': doctors})
-=======
     from .models import Lekarz
     lekarze = Lekarz.objects.all()
     return render(request, 'bazam/lista_doktorow.html', {'lekarze': lekarze})
->>>>>>> dd35c92cf55a5238c56010203219ba43ec8effb8
