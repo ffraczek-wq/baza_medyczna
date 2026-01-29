14e1e8d4ec1a448532e1b55897ef58a44344bf8b
jrepelowicz <jrepelowicz@st.swps.edu.pl>
2026-01-29 18:12:51 +0100

    s glowna

--- baza_medyczna_project/bazam/views.py
+++ baza_medyczna_project/bazam/views.py
@@ -4,3 +4,6 @@ def lista_doktorow(request):
     from .models import Lekarz
     lekarze = Lekarz.objects.all()
     return render(request, 'bazam/lista_doktorow.html', {'lekarze': lekarze})
+
+def strona_glowna(request):
+    return render(request, 'bazam/strona_glowna.html')
