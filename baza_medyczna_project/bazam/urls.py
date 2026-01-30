875140462fe9449e21021b1d8b5c27f71e951012
Franciszek <ffraczek@st.swps.edu.pl>
2026-01-30 09:49:42 +0100

    mojo

--- baza_medyczna_project/bazam/urls.py
+++ baza_medyczna_project/bazam/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('', views.strona_glowna, name='strona_glowna'),
 
     path('lekarze/', views.lista_doktorow, name='lista_doktorow'),
+
+    path('zabiegi/', views.lista_zabiegow, name='lista_zabiegow'),
+
+    path('koszyk/dodaj/<int:id>/', views.dodaj_do_koszyka, name='dodaj-do-koszyka'),
+    
+    path('koszyk/', views.pokaz_koszyk, name='pokaz-koszyk'),
 ]
--- baza_medyczna_project/bazam/views.py
+++ baza_medyczna_project/bazam/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Zabiegi
 
 def lista_doktorow(request):
     from .models import Lekarz
@@ -7,3 +8,25 @@ def lista_doktorow(request):
 
 def strona_glowna(request):
     return render(request, 'bazam/strona_glowna.html')
+
+def lista_zabiegow(request):
+    zabiegi = Zabiegi.objects.all()
+    return render(request, 'bazam/lista_zabiegow.html', {'zabiegi': zabiegi})
+
+
+
+
+def dodaj_do_koszyka(request, id):
+    koszyk = request.session.get('koszyk', [])
+    koszyk.append(id)
+    request.session['koszyk'] = koszyk
+    return redirect('lista_zabiegow')
+
+def pokaz_koszyk(request):
+    ids_w_koszyku = request.session.get('koszyk', [])
+    wybrane_zabiegi = Zabiegi.objects.filter(id__in=ids_w_koszyku)
+    suma = sum(zabieg.koszt for zabieg in wybrane_zabiegi)
+    return render(request, 'bazam/koszyk.html', {
+    'zabiegi': wybrane_zabiegi, 
+    'suma': suma
+    })
